9278da4899855789f03f8750452d7c1c1c69f1e5
Zero <249002031@qq.com>
2020-03-25 23:29:45 +0800

    添加介质边界组件

---
+++ Assets/Scenes/Script/MediumInterface.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediumInterface : MonoBehaviour
+{
+
+    public string inside;
+
+    public string outside;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
